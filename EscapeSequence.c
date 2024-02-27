163ec6d1c177e302d89b0194def75d4a4cfd0703
Deepayan Das <145363958+Deepayan80@users.noreply.github.com>
2024-02-27 11:49:41 +0530

    Add files via upload

--- EscapeSequence.c
+++ EscapeSequence.c
@@ -6,7 +6,9 @@ int main()
     // printf("Roll : 12\n");
     // printf("\tName\tAge\n");
     // printf("\tDeep\t22\n");
-    printf("Sir\n");
-    printf("\tHow are you\n");
+    // printf("Sir\n");
+    // printf("\tHow are you\n");
+    printf("Deepayan Das\r");
+    printf("Sahajan");
     return 0;
 }

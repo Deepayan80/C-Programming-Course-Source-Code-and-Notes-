b98654b52f0416124327f317591bce70e008e669
Deepayan Das <145363958+Deepayan80@users.noreply.github.com>
2024-03-14 13:08:13 +0530

    Add files via upload

---
+++ debug.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("HI, how are you ?\n");
+    printf("I am fine !\n");
+    printf("\\n is a new line character\n");
+    printf("MingW\\bin");
+    return 0;
+}
